8e7a819c0678f15fa4c5e4123a897cc90c38fb51
Shreyashi0719 <mallashreyashi2005@gmail.com>
2026-08-05 23:19:09 +0530

    Time: 0 ms (100%), Space: 8.4 MB (86.98%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+         int i = s.size() - 1;
+         int j = t.size() - 1;
+         int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+
+            // Find next valid char in s
+            while (i >= 0) {
+                if (s[i] == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+
+            // Find next valid char in t
+            while (j >= 0) {
+                if (t[j] == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+
+            // Compare characters
+            if (i >= 0 && j >= 0) {
+                if (s[i] != t[j]) return false;
+            } else {
+                // One string ended before the other
+                if (i >= 0 || j >= 0) return false;
+            }
+
+            i--;
+            j--;
+        }
+
+        return true;
+        
+    }
+};
