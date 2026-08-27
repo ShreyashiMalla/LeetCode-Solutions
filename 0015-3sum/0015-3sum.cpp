9538d47115bf03c1d51af40150c6bd5ed1b89bbe
Shreyashi0719 <mallashreyashi2005@gmail.com>
2026-08-27 08:06:51 +0530

    Time: 59 ms (19.2%), Space: 29.2 MB (28.45%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        int n = nums.size();
-        
-        sort(nums.begin(), nums.end());
-
-        for(int i = 0; i < n; i++) {
-            // Skip duplicate fixed elements
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while(left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if(sum == 0) {
-                    ans.push_back({nums[i], nums[left], nums[right]});
-
-                    left++;
-                    right--;
-
-                    // Skip duplicate left
-                    while(left < right && nums[left] == nums[left - 1]) left++;
-
-                    // Skip duplicate right
-                    while(left < right && nums[right] == nums[right + 1]) right--;
+        vector<vector<int>> res;
+        int n= nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int l=i+1,r=n-1;
+            while(l<r){
+                long long sum=(long long)nums[i]+nums[l]+nums[r];
+                if (sum==0){
+                    res.push_back({nums[i],nums[l],nums[r]});
+                    while(l<r && nums[l]==nums[l+1]) l++;
+                    while(l<r && nums[r]==nums[r-1]) r--;
+                    l++;
+                    r--;
                 }
-                else if(sum < 0) {
-                    left++;  // need bigger sum
+                else if (sum<0){
+                    l++;
                 }
-                else {
-                    right--; // need smaller sum
-                }
+                else{
+                    r--;
+                }  
             }
         }
-
-        return ans;
-        
+        return res;        
     }
 };
